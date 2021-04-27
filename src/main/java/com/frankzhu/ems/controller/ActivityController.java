d8a0b5cac44f06103ed93fd6eb69a6eac42c165b
elizabeth <2278817925@qq.com>
2021-04-27 17:00:29 +0800

    commit1.0

--- src/main/java/com/frankzhu/ems/controller/ActivityController.java
+++ src/main/java/com/frankzhu/ems/controller/ActivityController.java
@@ -3,11 +3,12 @@ package com.frankzhu.ems.controller;
 import com.frankzhu.ems.mapper.ActivityMapper;
 import com.frankzhu.ems.model.Activity;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @Api(tags = "活动预约管理")
 @RestController
@@ -20,7 +21,7 @@ public class ActivityController {
     }
 
     @PostMapping("api/activity/reserve")
-    @ApiOperation("活动预约")
+    @ApiOperation("住户进行活动预约")
     public Integer ReserveActivity(@RequestBody Map<String,Object> params){
         String status = "待处理";
         String date = params.get("date").toString();
@@ -29,47 +30,86 @@ public class ActivityController {
         String startTime = params.get("startTime").toString();
         String endTime = params.get("endTime").toString();
         String room_id = params.get("room_id").toString();
-//        room_id = room_id.substring(0,room_id.length()-2);
-//        resident_id = resident_id.substring(0,resident_id.length()-2);
         return activityMapper.reserveActivity(new Activity(room_id,resident_id,usage,date,startTime,endTime,status));
     }
-   //住户获取所有活动预约（根据room分）
-    @GetMapping("api/activity/all")
-    @ApiOperation("获取所有活动预约")
-    public List<Map<String,Object>> residentFindAllActivity(){
-        return activityMapper.findAllActivity();
-    }
-
-
-//    //住户获取所有活动预约（根据room分）
-//    @GetMapping("api/activity/all")
-//    @ApiOperation("获取所有活动预约")
-//    public List<Map<String,Object>> residentFindAllActivity(){
-//        return activityMapper.findAllActivity();
-//    }
-
-
-//
-//
-//    //管理员获取所有活动预约
-//    @GetMapping("api/activity/all")
-//    @ApiOperation("获取所有活动预约")
-//    public List<Map<String,Object>> adminFindAllActivity(){
-//        return activityMapper.findallActivity();
-//    }
-//
-//    //管理员设置活动预约的状态
-//    @GetMapping("api/activity/all")
-//    @ApiOperation("获取所有活动预约")
-//    public List<Map<String,Object>> setActivityStatus(){
-//        return activityMapper.findallActivity();
-//    }
-
 
 
     @GetMapping("/api/activity/cancel")
     @ApiOperation("取消活动预约")
+    @ApiImplicitParam(name="activity_id",value = "活动id",required = true,paramType = "query",dataType = "String")
     public Integer deleteActivity(@RequestParam(value ="activity_id", defaultValue = "") String activity_id){
         return activityMapper.deleteActivity(activity_id);
     }
+
+   //住户获取所有活动预约（根据room分）
+    @GetMapping("api/activity/allInfo")
+    @ApiOperation("住户获取所有活动预约")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "room_id",value = "房间号",required = true,paramType = "query",dataType = "String"),
+            @ApiImplicitParam(name = "pageSize",value = "单页数",required = true,paramType = "query",dataType = "String"),
+            @ApiImplicitParam(name = "currentPage",value = "当前页",required = true,paramType = "query",dataType = "String")
+    })
+    public Map<String, Object> residentFindAllActivity(
+            @RequestParam(value = "room_id", defaultValue = "") String room_id,
+            @RequestParam(value = "pageSize", defaultValue = "") Integer pageSize,
+            @RequestParam(value = "currentPage", defaultValue = "") Integer currentPage){
+        Integer allNum = pageSize*(currentPage-1);
+        List<Map<String, Object>> activityInformation = activityMapper.residentFindAllActivity(pageSize,allNum,room_id);
+        int totalNum = activityMapper.residentFindActivityNum(room_id);
+        Map<String, Object> activity =new HashMap<String, Object>();
+        activity.put("totalNum", totalNum);
+        activity.put("activityInfo",activityInformation);
+        return activity;
+    }
+
+    //管理员获取所有活动预约（根据room/status分）
+    @GetMapping("api/activity/all")
+    @ApiOperation("住户获取所有活动预约")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "room_id",value = "房间号",required = true,paramType = "query",dataType = "String"),
+            @ApiImplicitParam(name = "pageSize",value = "单页数",required = true,paramType = "query",dataType = "int"),
+            @ApiImplicitParam(name = "currentPage",value = "当前页",required = true,paramType = "query",dataType = "int"),
+            @ApiImplicitParam(name = "status",value = "审批状态",required = true,paramType = "query",dataType = "String")
+    })
+    public Map<String, Object> adminFindAllActivity(
+            @RequestParam(value = "room_id", defaultValue = "") String room_id,
+            @RequestParam(value = "pageSize", defaultValue = "") Integer pageSize,
+            @RequestParam(value = "currentPage", defaultValue = "") Integer currentPage,
+            @RequestParam(value = "status", defaultValue = "") String status){
+        Integer allNum = pageSize*(currentPage-1);
+        List<Map<String, Object>> activityInformation = new ArrayList<>();
+        Integer totalNum = 0;
+        System.out.println(room_id);
+        if (room_id.isEmpty()){
+            activityInformation = activityMapper.findAllActivityByStatus(pageSize,allNum,status);
+            totalNum = activityMapper.findActivityNumByStatus(status);
+        }else{
+            activityInformation = activityMapper.adminFindAllActivity(pageSize,allNum,room_id,status);
+            totalNum = activityMapper.adminFindActivityNum(room_id,status);
+        }
+        Map<String, Object> activity =new HashMap<String, Object>();
+        activity.put("totalNum", totalNum);
+        activity.put("activityInfo",activityInformation);
+        return activity;
+    }
+
+    //管理员设置活动预约的状态
+    @GetMapping("api/activity/approve")
+    @ApiOperation("设置活动预约状态")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "adminId",value = "管理员id",required = true,paramType = "query",dataType = "String"),
+            @ApiImplicitParam(name = "activity_id",value = "维修id",required = true,paramType = "query",dataType = "String"),
+            @ApiImplicitParam(name = "status",value = "审批状态",required = true,paramType = "query",dataType = "String")
+    })
+    public Integer setActivity(
+            @RequestParam(value = "adminId", defaultValue = "") String adminId,
+            @RequestParam(value = "activity_id", defaultValue = "") String activity_id,
+            @RequestParam(value = "status", defaultValue = "") String status){
+        System.out.println("设置预约状态");
+        System.out.println(adminId);
+        System.out.println(activity_id);
+        System.out.println(status);
+        return activityMapper.setActivityStatus(adminId,activity_id,status);
+    }
+
 }
--- src/main/java/com/frankzhu/ems/mapper/ActivityMapper.java
+++ src/main/java/com/frankzhu/ems/mapper/ActivityMapper.java
@@ -1,10 +1,7 @@
 package com.frankzhu.ems.mapper;
 
 import com.frankzhu.ems.model.Activity;
-import org.apache.ibatis.annotations.Delete;
-import org.apache.ibatis.annotations.Insert;
-import org.apache.ibatis.annotations.Param;
-import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.*;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -18,14 +15,42 @@ public interface ActivityMapper {
             "(#{room_id}, #{resident_id}, #{date}, #{startTime}, #{endTime}, #{usage},#{status})")
     Integer reserveActivity(Activity activity);
 
-    // 获取所有活动信息
-    @Select("select activity_id, A.resident_id as resident_id ,room_usage,name as resident_name,date,startTime,endTime,status from Room join Activity A join Resident R on " +
-            "A.resident_id = R.resident_id and Room.room_id = A.room_id")
-    List<Map<String,Object>> findAllActivity();
+    // 用户获取所有活动信息（分房间分页）
+    @Select("select activity_id, A.resident_id as resident_id,activity_usage,admin_name,room_usage,name as resident_name,date,startTime,endTime," +
+            "status from Room join Activity A on Room.room_id = A.room_id  join Resident R2 on A.resident_id = R2.resident_id " +
+            "left join Admin A2 on A.admin_id = A2.admin_id where A.room_id =#{room_id} limit #{pageSize} OFFSET #{allNum}")
+    List<Map<String,Object>> residentFindAllActivity(@Param("pageSize") int pageSize,@Param("allNum") int allNum,@Param("room_id") String room_id);
 
     // 取消活动预约
     @Delete("delete from Activity where activity_id = #{activity_id}")
     Integer deleteActivity(@Param("activity_id") String activity_id);
 
+    //用户获取活动信息数量
+    @Select("select count(*) from Activity where room_id = #{room_id}")
+    Integer residentFindActivityNum(@Param("room_id") String room_id);
+
+    // 管理员获取所有活动信息
+    @Select("select activity_id, A.resident_id as resident_id,activity_usage,admin_name,room_usage,name as resident_name,date,startTime,endTime," +
+            "status from Room join Activity A on Room.room_id = A.room_id  join Resident R2 on A.resident_id = R2.resident_id "+
+            "left join Admin A2 on A.admin_id = A2.admin_id where A.room_id =#{room_id} and status=#{status} limit #{pageSize} OFFSET #{allNum}")
+    List<Map<String,Object>> adminFindAllActivity(@Param("pageSize") int pageSize,@Param("allNum") int allNum,@Param("room_id") String room_id,@Param("status") String status);
+
+    // 管理员获取活动信息的数量
+    @Select("select count(*) from Activity where room_id = #{room_id} and status=#{status}")
+    Integer adminFindActivityNum(@Param("room_id") String room_id,@Param("status") String status);
+
+    //根据status获取
+    @Select("select activity_id, A.resident_id as resident_id,activity_usage,admin_name,room_usage,name as resident_name,date,startTime,endTime," +
+            "status from Room join Activity A on Room.room_id = A.room_id  join Resident R2 on A.resident_id = R2.resident_id " +
+            "left join Admin A2 on A.admin_id = A2.admin_id where status=#{status} limit #{pageSize} OFFSET #{allNum}")
+    List<Map<String,Object>> findAllActivityByStatus(@Param("pageSize") int pageSize,@Param("allNum") int allNum,@Param("status") String status);
+
+    //根据status获取 数量
+    @Select("select count(*) from Activity where status=#{status}")
+    Integer findActivityNumByStatus(@Param("status") String status);
+
+    //设置活动状态
+    @Update("update Activity set status =#{status}, admin_id=#{adminId} where activity_id=#{activity_id}")
+    Integer setActivityStatus(@Param("adminId") String adminId, @Param("activity_id") String repair_id,@Param("status") String status);
 
 }

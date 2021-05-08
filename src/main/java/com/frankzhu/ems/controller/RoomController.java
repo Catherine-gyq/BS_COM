1e8ee90dcf78b71c1129cfd4c65a5504eeddfdc8
elizabeth <2278817925@qq.com>
2021-05-08 19:48:30 +0800

    commit1.3

--- src/main/java/com/frankzhu/ems/controller/RoomController.java
+++ src/main/java/com/frankzhu/ems/controller/RoomController.java
@@ -2,6 +2,9 @@ package com.frankzhu.ems.controller;
 
 import com.frankzhu.ems.mapper.RoomMapper;
 import com.frankzhu.ems.model.Resident;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -23,6 +26,18 @@ public class RoomController {
     public List<Map<String, Object>> FindAllRoom(){
         return roomMapper.findAllRoom();
     }
+
+//    删除该房间
+    @GetMapping("/api/room/delete")
+    @ApiOperation("删除消息")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "roomId",value = "房间id",required = true,paramType = "query",dataType = "String"),
+    })
+    public Integer deleteRoomByNo(@RequestParam(value = "roomId", defaultValue = "") String roomId){
+        return roomMapper.deleteRoomById(roomId);
+    }
+
+//  添加房间信息
 
 
 }
--- src/main/java/com/frankzhu/ems/mapper/RoomMapper.java
+++ src/main/java/com/frankzhu/ems/mapper/RoomMapper.java
@@ -1,5 +1,6 @@
 package com.frankzhu.ems.mapper;
 
+import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Repository;
@@ -9,6 +10,12 @@ import java.util.Map;
 
 @Repository
 public interface RoomMapper {
+    //获取所有的房间信息
     @Select("select * from Room")
     List<Map<String, Object>> findAllRoom();
+
+    // 通过id删除房间信息
+    @Delete("delete from Room where room_id=#{roomId}")
+    Integer deleteRoomById(@Param("roomId") String roomId);
+
 }
